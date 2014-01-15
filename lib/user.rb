1d5fe51854b2cc3745c6607ab5c7d8dc3d5b7e5d
lacemaker <merlettaia@gmail.com>
2014-01-15 12:32:15 +0400

    added user states

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,13 @@
 class User
   attr_accessor :name
   attr_reader :is_busy
-
+  
+  state_machine :initial => :free do
+    state :in_game
+    state :disconnected
+    transition :free => :in_game, :on => :start_game
+    transition all => :disconnected, :on => :disconnect
+  end
   def initialize(name)
     @name = name
   end
