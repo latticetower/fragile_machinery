0a84f2f51cda6c2b3aa817cb7a00068176a785f3
lacemaker <merlettaia@gmail.com>
2014-01-16 12:08:42 +0400

    added basic page with chat. moved to websocket, now chat works ok

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "http://www.rubygems.org"
 
 gem 'eventmachine'
 gem 'state_machine'
+gem 'em-websocket'
 
---
+++ websocket_chat_server.rb
@@ -0,0 +1,30 @@
+require 'em-websocket'
+# websocket server based on simple example from em-websocket page
+
+chat_host = "0.0.0.0"
+chat_port = 8080
+chat_host = ARGV[0] if ARGV.size > 0
+chat_port = ARGV[1].to_i if ARGV.size > 1
+
+EM.run {
+  EM::WebSocket.run(:host => chat_host, :port => chat_port) do |ws|
+    ws.onopen { |handshake|
+      puts "Someone opened WebSocket connection"
+      puts handshake
+      puts self
+      puts ws
+      # Access properties on the EM::WebSocket::Handshake object, e.g.
+      # path, query_string, origin, headers
+
+      # Publish message to the client
+      ws.send "Hello Client, you connected to #{handshake.path}"
+    }
+
+    ws.onclose { puts "Connection closed" }
+
+    ws.onmessage { |msg|
+      puts "Recieved message: #{msg}"
+      ws.send "Pong: #{msg}"
+    }
+  end
+}
